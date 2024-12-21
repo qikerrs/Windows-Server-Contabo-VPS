a2c71eba5afbcf476d3613be5edfcd459cb7ee58
qikerrs <129662698+qikerrs@users.noreply.github.com>
2024-12-22 05:33:57 +0700

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -72,7 +72,7 @@ rsync -avz --progress winfile/* /mnt
 
 umount winfile
 
-wget -O virtio.iso https://bit.ly/4d1g7Ht
+wget -O virtio.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" https://bit.ly/49LXm9L
 
 mount -o loop virtio.iso winfile
 
